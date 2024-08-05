c8f4d08d0a16c8ec7a78a390637841e51e35b628
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2024-08-05 21:56:57 +0300

    first commit

---
+++ src/app/test-component/test-component.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-test-component',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './test-component.component.html',
+  styleUrl: './test-component.component.css'
+})
+export class TestComponentComponent {
+  dataArray = [
+    {
+      id: 0,
+      name: "Lachezar",
+      age: 22,
+      height: 1.81
+    },
+    {
+      id: 1,
+      name: "Ivan",
+      age: 18,
+      height: 1.85
+    },
+    {
+      id: 2,
+      name: "Anq",
+      age: 23,
+      height: 1.67
+    },
+  ];
+
+
+  clickHandler() {
+    console.log("Clicked");
+  }
+}
