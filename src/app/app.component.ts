2ed17ab57612fb494f01237cfd442e0052842adb
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2024-08-08 23:00:52 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 //import testComponent from './test-component/test-component.component';
 import { TestComponentComponent } from './test-component/test-component.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestComponentComponent],
+  imports: [RouterOutlet, TestComponentComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/destination-card/destination-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destination-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './destination-card.component.html',
+  styleUrl: './destination-card.component.css'
+})
+export class DestinationCardComponent {
+
+}
